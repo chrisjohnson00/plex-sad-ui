da1db4c85f57b2c9f76f9959ab8c79a5880a9579
Chris Johnson <chrisjohnson00@hotmail.com>
2024-01-02 17:01:14 -0800

    chore: adding health endpoint

--- sad_ui/application.py
+++ sad_ui/application.py
@@ -1,10 +1,11 @@
+import os
+
 from flask import (
     Blueprint, render_template
 )
 import sad_libraries.redis as sad_redis
 import sad_ui.constants as sad_constants
 import sad_libraries.tmdb as sad_tmdb
-import logging
 
 bp = Blueprint('app', __name__, url_prefix='/')
 
@@ -14,4 +15,11 @@ def index():
     search_keys = sad_redis.get_from_cache(key=sad_constants.SAD_SEARCH_KEYS)
     search_results = sad_redis.get_from_cache(key=sad_constants.SAD_RESULTS)
     tmdb_config = sad_tmdb.get_config()
-    return render_template('app/list.html', search_keys=search_keys, search_results=search_results, tmdb_config=tmdb_config)
+    return render_template('app/list.html', search_keys=search_keys, search_results=search_results,
+                           tmdb_config=tmdb_config)
+
+
+@bp.route('/health')
+def health():
+    version = os.getenv('VERSION')
+    return render_template('app/health.html', version=version)
