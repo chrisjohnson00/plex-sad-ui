7907590346fcea575d6629d67f93b7d9cb85116c
Chris Johnson <chrisjohnson00@hotmail.com>
2024-01-06 22:33:22 -0800

    feat: adding refresh via pulsar messages (#4)

--- README.md
+++ README.md
@@ -3,26 +3,24 @@ The UI component of my Plex Search and Destroy solution
 
 # PyPi Dependency updates
 
+`pulsar-client` does not support Python > 3.10
+
 ```shell
-docker run -it --rm -v ${PWD}:/repo -w /repo python:3.12-slim bash
+docker run -it --rm -v ${PWD}:/repo -w /repo python:3.10-slim bash
 apt update && apt install -y git
 pip install --upgrade pip
-pip install --upgrade Flask gunicorn 'sad_libraries@git+https://github.com/chrisjohnson00/plex-sad-libraries.git@v0.1.3'
+pip install --upgrade Flask gunicorn pulsar-client 'sad_libraries@git+https://github.com/chrisjohnson00/plex-sad-libraries.git@v0.1.3'
 pip freeze > requirements.txt
 ```
 
 ## Running locally
 
 ```commandline
+export TMDB_API_ACCESS_TOKEN='your token'
+export PULSAR_SERVER=192.168.1.133
+export PULSAR_TOPIC=test-topic
 export FLASK_APP=sad_ui
 flask --debug run
-```
-
-or
-
-```shell
-docker build . -t sad-ui
-docker run -it --rm -p 5000:5000 sad-ui
 ```
 
 Run redis locally with
--- sad_ui/application.py
+++ sad_ui/application.py
@@ -1,11 +1,14 @@
+import json
 import os
 
-from flask import (
-    Blueprint, render_template
-)
+import pulsar
 import sad_libraries.redis as sad_redis
-import sad_ui.constants as sad_constants
 import sad_libraries.tmdb as sad_tmdb
+from flask import (
+    Blueprint, render_template, request, current_app as app
+)
+
+import sad_ui.constants as sad_constants
 
 bp = Blueprint('app', __name__, url_prefix='/')
 
@@ -23,3 +26,26 @@ def index():
 def health():
     version = os.getenv('VERSION')
     return render_template('app/health.html', version=version)
+
+
+@bp.route('/request-search')
+def request_search():
+    search_key = request.args.get('search_key', default=None, type=str)
+    app.logger.debug(f"Received search request for '{search_key}'")
+    # Create a Pulsar client
+    client = pulsar.Client(f'pulsar://{os.environ["PULSAR_SERVER"]}')
+
+    # Create a producer on the topic 'plex-search'
+    producer = client.create_producer(os.environ['PULSAR_TOPIC'])
+
+    # Create a message
+    message = [search_key]
+    message_json = json.dumps(message).encode('utf-8')
+
+    # Send the message
+    producer.send(message_json)
+
+    # Close the producer and client to free up resources
+    producer.close()
+    client.close()
+    return "OK"
